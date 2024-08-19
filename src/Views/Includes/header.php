ec71ec044b4e0afc1c64e12c95a674292ce72cb5
Elodie <elodie.loeb.simplon@gmail.com>
2024-08-19 16:48:32 +0200

    debutHeader

---
+++ src/Views/Includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galactic Restaurant</title>
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+</head>
+
+<body>
+    <header>
+        <div class="header backgroundHeader center">
+            <img class="logoHeader" src="assets/imgs/Logo.png" alt="logo du restaurant galactic">
+            <h1> L'étoile dorée </h1>
+        </div>
+    </header>
--- src/Views/accueil.php
+++ src/Views/accueil.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once __DIR__ . '/Includes/header.php';
+ 
 ?>
 
 <div>
