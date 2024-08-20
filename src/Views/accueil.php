bbd820e5032b97b1e1be7cc9d53c261ae0aba167
Tom <xnosx@hotmail.fr>
2024-08-20 12:10:01 +0200

    Finish illustration section

--- src/Views/accueil.php
+++ src/Views/accueil.php
@@ -13,7 +13,16 @@ include_once __DIR__ . '/Includes/header.php';
     </section>
     <section class="illustration-section">
         <img class="illustration" src="assets/imgs/Equipe/ChefPoulpe.png" alt="spheres-dish-illustration">
-         <p>"Rencontrez le chef Poulpe : un expert en gastronomie spatiale dont les tentacules agiles sont aussi talentueux pour mélanger les saveurs que pour attraper les étoiles filantes. 
-         À L'Étoile Dorée, il prépare des plats qui font voyager vos papilles tout en restant gravitationnellement délicieux !"</p>
+        <p>
+            "Rencontrez le chef Poulpe : un expert en gastronomie spatiale dont les tentacules agiles sont aussi talentueux pour mélanger les saveurs que pour attraper les étoiles filantes.<br>
+            À L'Étoile Dorée, il prépare des plats qui font voyager vos papilles tout en restant gravitationnellement délicieux !"
+        </p>
+        <div class="primary-title-back-stroke">- L'ETOILE DOREE -</div>
+        <div class="big-reservation-title">Book a Table</div>
+        <p id="bottom-illustration-text">
+            Aliquet egestas gravida orci nulla lacus suspendisse. Vel ullamcorper ultrices volutpat enim dignissim id purus. Urna quam massa urna dolor enim dui lectus id. 
+            Tristique dignissim pharetra feugiat mattis non. Vivamus libero feugiat odio eget potenti.
+        </p>
+        <div id="reservation-button">Book</div>
     </section>
 </main>
