951232bbab04120b234b071bdfc2e287658738ac
Tom <xnosx@hotmail.fr>
2024-08-21 16:00:47 +0200

    Improve form styling

--- src/Views/reservation.php
+++ src/Views/reservation.php
@@ -1,7 +1,7 @@
 <?php include_once __DIR__ . '/Includes/header.php'; ?>
 
-<section class="reservation-form-container">
-  <form action="" method="post">
+<div class="reservation-form-contact-container">
+  <form action="#" method="post">
     <span class="yellow-text ntr-title">Réservation</span>
     <h2 class="ntr-title">Réserver une table</h2>
     <div class="form-group">
@@ -14,10 +14,21 @@
       <label for="time">Heure</label>
       <input type="time" class="form-control" id="time" name="time">
       <label for="number">Nombre de convives</label>
-      <input type="number" class="form-control" id="number" name="number"></label>
+      <input type="number" class="form-control" id="number" name="number">
     </div>
     <div id="form-reservation-button-final" onclick="location.href='ProcessReservation'">Réserver</div>
   </form>
-</section>
+  <section>
+    <span class="yellow-text ntr-title">L'Étoile dorée</span>
+    <h2 class="ntr-title">La destination ultime pour une cuisine de qualité.</h2>
+    <p>Aliquet egestas gravida orci nulla lacus suspendisse. Vel ullamcorper ultrices volutpat enim dignissim id purus. </p>
+    <span class="yellow-text ntr-title">Téléphone</span>
+    <p>+33 1 23 45 67 89</p>
+    <span class="yellow-text ntr-title">Email</span>
+    <p>contact@letoiledoree.com</p>
+    <span class="yellow-text ntr-title">Adresse</span>
+    <p>8911 Tanglewood Ave.Capitol Heights, MD 20743</p>
+  </section>
+</div>
 
 <?php include_once __DIR__ . '/Includes/footer.php'; ?>
