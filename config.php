9a4a65cc189fded01c4f3c34bd782b47cc7de3cd
Tom <xnosx@hotmail.fr>
2024-08-23 16:40:23 +0200

    Add constant for full url

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ define('MAX_GUESTS', 60);
 
 // EN LOCAL :
 define('HOME_URL', '/');
+define('FULL_URL', 'http://your-url-here/'); // For mails
 
 // Ne pas toucher :
 define('DB_INITIALIZED', TRUE);
