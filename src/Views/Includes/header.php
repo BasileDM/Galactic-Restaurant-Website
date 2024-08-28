042d4ab6cba1b9184d2834c70f7ce69879eec703
Tom <xnosx@hotmail.fr>
2024-08-28 16:41:55 +0200

    Fix seats script

--- src/Views/Includes/header.php
+++ src/Views/Includes/header.php
@@ -9,10 +9,10 @@ $current_page = basename($_SERVER['REQUEST_URI'], ".php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L'Étoile Dorée</title>
-    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/global.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script type="module" defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script defer src="assets/js/script.js"></script>
+    <script defer src="./assets/js/script.js"></script>
     <?php
     if (isset($_SESSION['connecte']) && $_SESSION['connecte'])
     { ?>
